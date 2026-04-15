7468699fa7a37b90d93d8618ab7320fdf294d6ac
Your Name <you@example.com>
2026-04-15 04:32:19 +0000

    .

---
+++ examples/embeddedversion/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/4vun1t/bine/process/embedded"
+)
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
+	p, err := embedded.NewCreator().New(context.Background(), "--version")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Starting...\n")
+	if err = p.Start(); err != nil {
+		return err
+	}
+	fmt.Printf("Waiting...\n")
+	return p.Wait()
+}
---
+++ examples/go.mod
@@ -0,0 +1,12 @@
+module github.com/4vun1t/bine/examples
+
+go 1.15
+
+require (
+	github.com/4vun1t/bine v0.2.0
+	github.com/golang/protobuf v1.5.2
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5
+	google.golang.org/grpc v1.38.0
+)
+
+replace github.com/4vun1t/bine => ../
